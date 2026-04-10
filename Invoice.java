d26f5cdaf69af30d5a2d2ca71469875d13d15cb8
Yousif mansour <yousef.m8923@gmail.com>
2026-04-10 16:33:08 +0200

    first commit

--- Invoice.java
+++ Invoice.java
@@ -1,1 +1,51 @@
+import java.time.LocalDate;
+enum PaymentMethod{
+    CASH, CREDIT_CARD, ONLINE
+}
+public class Invoice {
+private double totalAmount;
+private LocalDate paymentDate;
+private PaymentMethod method;
 
+
+    public Invoice() {
+    }
+
+    public PaymentMethod getMethod() {
+        return this.method;
+    }
+
+    public void setMethod(final PaymentMethod method) {
+        this.method = method;
+    }
+
+    public Invoice(LocalDate paymentDate, double totalAmount) {
+        this.paymentDate = paymentDate;
+        this.totalAmount = totalAmount;
+    }
+
+    public LocalDate getPaymentDate() {
+        return this.paymentDate;
+    }
+
+    public void setPaymentDate( LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getTotalAmount() {
+        return this.totalAmount;
+    }
+
+    public void setTotalAmount( double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+public void showPaymentMethods(){
+    System.out.println("----payment methods----");
+    System.out.println("CASH\nCREDIT_CARD\nONLINE");
+}
+
+
+
+
+}
