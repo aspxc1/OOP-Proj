f7602d87b59ef470f80e4b9678202a6fb6db09d6
Yousif mansour <yousef.m8923@gmail.com>
2026-04-10 16:33:08 +0200

    first commit

--- Reservation.java
+++ Reservation.java
@@ -1,1 +1,65 @@
+import java.time.LocalDate;
 
+public class Reservation {
+
+    enum ReservationStatus{
+        PENDING, CONFIRMED, CANCELLED, COMPLETED
+    }
+
+private String roomRefrence;
+private String guestRefrence;
+private LocalDate checkInDate;
+private LocalDate checkOutDate;
+  private ReservationStatus reservationStatus;
+
+    public Reservation( String LocalDate,  LocalDate checkOutDate,  String guestRefrence,  String roomRefrence) {
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.guestRefrence = guestRefrence;
+        this.roomRefrence = roomRefrence;
+    }
+
+    public Reservation() {
+    }
+
+    public ReservationStatus getReservationStatus() {
+        return this.reservationStatus;
+    }
+
+    public void setReservationStatus(final ReservationStatus reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
+
+    public LocalDate getCheckInDate() {
+        return this.checkInDate;
+    }
+
+    public void setCheckInDate( LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return this.checkOutDate;
+    }
+
+    public void setCheckOutDate( LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getGuestRefrence() {
+        return this.guestRefrence;
+    }
+
+    public void setGuestRefrence( String guestRefrence) {
+        this.guestRefrence = guestRefrence;
+    }
+
+    public String getRoomRefrence() {
+        return this.roomRefrence;
+    }
+
+    public void setRoomRefrence( String roomRefrence) {
+        this.roomRefrence = roomRefrence;
+
+    }
+}
